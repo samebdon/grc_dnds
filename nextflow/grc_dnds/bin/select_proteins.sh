07c56b9a750a9fce1082530f931cc08977082552
Sam Ebdon <samebdon@gmail.com>
2024-11-12 17:40:06 +0000

    select_proteins.sh

--- nextflow/grc_dnds/bin/select_proteins.sh
+++ nextflow/grc_dnds/bin/select_proteins.sh
@@ -4,8 +4,9 @@ GFF="$1"
 FASTA="$2"
 
 awk '$3=="transcript"' | awk '{print $9}' | cut -f-1 -d';' | cut -f2- -d'=' $GFF > transcripts.txt
+awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < $FASTA > single_line.fa
 
 for TRANSCRIPT in transcripts.txt;
 do
-	awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' $FASTA | grep 'gene' -A1
+	grep -A1 $TRANSCRIPT single_line.fa
 done
